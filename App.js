de8175cca198c3b784ae1620e86ca41803f88fc4
khoirudhien86 <khoirudhien86.2@gmail.com>
2021-02-18 22:32:59 +0700

    membuat gopay bar

--- App.js
+++ App.js
@@ -17,6 +17,31 @@ export default class App extends Component {
               <Image style={{ marginLeft:15, height: 31}} source={require('./assets/promo.png')} />
             </View>
           </View>
+          {/* main gopay bar */}
+          <View>
+            <View style={{height: 40, flexDirection: 'row', justifyContent: 'space-between',marginHorizontal: 20, backgroundColor: 'blue', paddingHorizontal: 5, marginTop: 10, borderTopLeftRadius: 3, borderTopRightRadius: 3, alignItems: 'center'}}>
+              <Image source={require('./assets/gopay.png')} />
+              <Text style={{fontSize: 16, fontWeight: 'bold', color: 'white'}}>Rp 50.000</Text>
+            </View>
+            <View style={{height: 80, backgroundColor: 'skyblue', marginHorizontal: 20, borderBottomLeftRadius: 3, borderBottomRightRadius: 3, flexDirection: 'row'}}>
+              <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 12}}>
+                <Image  style={{height: 35, width: 35}} source={require('./assets/pay.png')} />
+                <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14, marginTop: 2}}>Pay</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 12}}>
+                <Image  style={{height: 35, width: 35}} source={require('./assets/nearby.png')} />
+                <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14, marginTop: 2}}>Nearby</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 12}}>
+                <Image  style={{height: 35, width: 35}} source={require('./assets/topup.png')} />
+                <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14, marginTop: 2}}>Top Up</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 12}}>
+                <Image  style={{height: 35, width: 35}} source={require('./assets/more.png')} />
+                <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14, marginTop: 2}}>More</Text>
+              </View>
+            </View>
+          </View>
         </View>
         {/* ini bottom bar */}
         <View style={styles.bottomBar}>
